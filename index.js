3ee052e6cf4569f7affefb241091090cf2e80508
Nes <neel@anywhere.co>
2021-05-07 09:40:51 +0530

    Changed output

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const github = require('@actions/github')
 try{
   const nameToGreet = core.getInput('whom-to-greet');
   console.log(`Hello, ${nameToGreet}`);
-  core.setOutput("time", new Date().toTimeString());
+  core.setOutput("time", (new Date()).toTimeString());
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload : ${payload}`);
 }catch(error){
