6fbaac52b8dd158b20179219ea5b05a1b2ded3ab
Nes <neel@anywhere.co>
2021-05-07 09:08:46 +0530

    Commit First Action

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require('@actions/core')
+const github = require('@actions/github')
 
-
-// most @actions toolkit packages have async methods
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+try{
+  const nameToGreet = core.getInput('whom-to-greet');
+  console.log(`Hello, ${nameToGreet}`);
+  core.setOutput(new Date().toTimeString());
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload : ${payload}`);
+}catch(error){
+  core.setFailed(error.message);
 }
-
-run();
