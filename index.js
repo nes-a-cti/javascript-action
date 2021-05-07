f235d2194384220403b8fecc75849b986083ea7a
Nes <neel@anywhere.co>
2021-05-07 18:05:21 +0530

    Checking Dependencies Confict

--- index.js
+++ index.js
@@ -22,6 +22,17 @@ const core = require('@actions/core');
 
 const source = ".";
 
+const requiredDependencies = new Set();
+requiredDependencies.add('com.google.guava:guava:29.0-jre');
+requiredDependencies.add('com.google.guava:failureaccess:1.0.1');
+requiredDependencies.add('com.google.guava:listenablefuture:9999.0-empty-to-avoid-conflict-with-guava');
+requiredDependencies.add('com.google.code.findbugs:jsr305:3.0.2');
+requiredDependencies.add('org.checkerframework:checker-qual:2.11.1');
+requiredDependencies.add('com.google.errorprone:error_prone_annotations:2.3.4');
+requiredDependencies.add('com.google.j2objc:j2objc-annotations:1.3');
+requiredDependencies.add('junit:junit:4.13');
+requiredDependencies.add('org.hamcrest:hamcrest-core:1.3');
+
 async function run(){
     try{
         let cmdOut = "";
@@ -34,9 +45,7 @@ async function run(){
                 cmdOut += data.toString();
             }
         }
-
         cmdOpts.ignoreReturnCode = true;
-
         let exitCode = await exec(command, cmdArgs, cmdOpts);
         let data = cmdOut;
         
@@ -44,13 +53,27 @@ async function run(){
 
         console.log(`Exit Code : ${exitCode}`);
 
-        // console.log(`Data : ${data}`);
-        findDependencies(data);
+        const foundDependency = findDependencies(data);
+        compareDependecies(foundDependency);
 
     }catch(error){
         core.setFailed(error.message);
     }
 
+}
+
+function compareDependecies(foundDependency){
+    const conflictedDepencies = new Set();
+    Array.from(foundDependency).every(value => {
+            if(!requiredDependencies.has(value)){
+                conflictedDepencies.add(value);
+                return false;
+            }
+            return true;
+    });
+
+    console.log(`Conflicted Dependencies Size : ${conflictedDepencies.size}`);
+    console.log(`Conflicted Dependencies : ${JSON.stringify(conflictedDepencies.size)}`);
 }
 
 function findDependencies(content){
@@ -66,14 +89,7 @@ function findDependencies(content){
             dependencies.add(ln);
         }
     }
-
-    console.log(`Length1 : ${dependencies.size}`);
-    console.log(`Length2 : `, dependencies.size);
-
     const val = [...dependencies].join(',');
-
-    console.log(`Dependeinces ${val}`);
-
 }
 
 module.exports = {run}
